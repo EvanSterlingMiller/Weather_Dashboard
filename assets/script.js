207c2a7563a39aad44f912c9f4276fa733be984a
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-03-20 18:53:20 -0400

    updates

--- assets/script.js
+++ assets/script.js
@@ -23,39 +23,7 @@ $("#search_button").on("click", function(){
     var query_url = "placeholder" + city + api_key
 
     
-    })
-    .then(function (response) {
-        console.log(response)
-        var temp_f =(response.main.temp - 273) * 1.8 +32
-        get_current_conditions(response)
-        get_current_conditions(response)
-        make_list()
-    })
-}) //ends search button
+  
 
-function make_list() {
-    var list_item = $("<li>").addClass("list_group_item").text(city)
-    $(".list").append(list_item)
-}
-
-function get_current_conditions(response) {
-    var temp_f = (response.main.temp -273) *1.8 + 32
-    temp_f = Math.floor(temp_f)
-
-    $("current_city").empty()
-
-    var card = $("<div>").addClass("card")
-    var card_body = $("<div>").addClass("card-body")
-    var city = $("<h2>").addClass("card-title").text(response.name)
-    var city_date =$("<h2>").addClass("card-title").text(date.toLocaleDateString('en-US'))
-    var temperature = $("<p>").addClass("card-text current-temp").text("Temperature is " + temp_f + " fahrenheit")
-    var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
-
-    city.append(city_date, image)
-    card_body.append(city, temperature)
-    card.append(card_body)
-    $("#current_city").append(card)
-
-}
 
 
